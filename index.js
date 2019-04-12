094a0cb05d6aa8071feb8d5fb78b6cc9ddbc1e95
oliviaxli12341 <43362216+oliviaxli12341@users.noreply.github.com>
2019-04-12 04:03:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -95,7 +95,6 @@ app.get('/tag/:tag', function(req, res) {
 });
 
 
-
-app.listen(3000, function() {
-    console.log('Listening on port 3000!');
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Listening!');
 });
